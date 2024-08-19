3f580a8e05198288b6df1f930dc300e868b33b50
AlirezaAhmadyan <ahmadyalireza111@gmail.com>
2024-08-19 10:16:27 +0330

    finish commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  content: ["./index.html"],
-  theme: {
-    extend: {
-      colors: {
-        'primary': 'hsl(47, 88%, 63%)',
-        'gray-500': 'hsl(0, 0%, 42%)',
-        'gray-900': 'hsl(0, 0%, 7%)',
-      }
+    mode: 'jit',
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            colors: {
+                'primary': 'hsl(47, 88%, 63%)',
+                'g-500': 'hsl(0, 0%, 42%)',
+                'g-950': 'hsl(0, 0%, 7%)',
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
